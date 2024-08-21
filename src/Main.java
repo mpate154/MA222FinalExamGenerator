fe55d215e0b0e9d840212b67301e24e2c96f64f1
Maya Yogesh Patel <147780592+mpate154@users.noreply.github.com>
2024-08-21 11:23:45 -0400

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        ExamMaker test = new ExamMaker(); //Creates an ExamMaker object which reads text files and stores all questions
-        test.generateExam(); //Prints our randomly generated exam
-    }
-}
